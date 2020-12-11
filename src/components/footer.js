cfe2154fff6a91e87cab3fe6c99d81b8fbe4e00a
BryantDavis1986 <Bryant.Davis@hotmail.com>
2020-12-10 17:52:42 -0800

    done, a statisfactory state

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,46 @@ import React from 'react';
 export default function Footer(props){
     return (
     <footer class="footer bg-black small text-center text-white-50">
+        <section class="contact-section bg-black">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Address</h4>
+                                <hr class="my-4" />
+                                <div class="small text-black-50">10642 1st Avenue SouthWest Seattle, Washington 98146</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <hr class="my-4" />
+                                <div class="small text-black-50"><a href="mailto:bryant.davis@hotmail.com?subject=E-Mail from your Portfolio">Bryant.Davis@hotmail.com</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Phone</h4>
+                                <hr class="my-4" />
+                                <div class="small text-black-50">+1 (206) 579-8547</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="social d-flex justify-content-center">
+                    <a class="mx-2" href="https://www.linkedin.com/in/bryantdavis-sd/" target='_blank'><i class="fab fa-linkedin"></i></a>
+                    <a class="mx-2" href="https://github.com/BryantDavis1986" target='_blank'><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+        </section><br/>
         <div class="container">Copyright © Your Website 2020</div>
         </footer>
     )
--- src/components/header.js
+++ src/components/header.js
@@ -2,21 +2,13 @@ import React from 'react';
 
 export default function Header(props){
     return (
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#projects">Projects</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#signup">Contact</a></li>
-                    </ul>
+                <header class="masthead">
+                <div class="container d-flex h-100 align-items-center">
+                    <div class="mx-auto text-center">
+                        <h1 class="mx-auto my-0 text-uppercase">Bryant Davis</h1>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Full Stack Software Engineer <br/> FrontEnd :JavaScript/React<br/>BackEnd:  .NET/C#</h2>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </header>
     )
 }
